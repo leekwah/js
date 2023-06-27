2320282de93e6d5b6f35fbf96d584c62b35c6e1d
leekwah <leekwahoon@gmail.com>
2023-06-27 22:20:03 +0900

    20230627
    20230627

--- js_udemy/01_starting-project/assets/scripts/app.js
+++ js_udemy/01_starting-project/assets/scripts/app.js
@@ -3,11 +3,11 @@ let currentResult = defaultResult;
 
 function add(num1, num2) {
   const result = num1 + num2;
+  return result;
 }
 
-currentResult = ((currentResult + 10) * 3) / 2 - 1;
+currentResult = add(1, 2);
 
 let calculationDescription = `( ${defaultResult} + 10) * 3) / 2 - 1`;
-let errorMessage = 'An error \n' + 'occurred!';
 
 outputResult(currentResult, calculationDescription);
